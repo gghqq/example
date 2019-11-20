26d07902e0fcda4ab81af96093a34fe3235b998d
gghqq <36395673+gghqq@users.noreply.github.com>
2019-11-20 15:14:00 +0800

    避免重复生产问题

--- src/main/java/concurrency/chapter6/ThreadInterrupt.java
+++ src/main/java/concurrency/chapter6/ThreadInterrupt.java
@@ -1,42 +1,30 @@
-/*    */ package concurrency.chapter6;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class ThreadInterrupt
-/*    */ {
-/*    */   public static void main(String[] args) throws InterruptedException {
-/* 15 */     Thread thread = new Thread()
-/*    */       {
-/*    */         public void run() {
-/*    */           while (true) {
-/*    */             try {
-/*    */               while (true)
-/* 21 */                 wait(100L);  break;
-/* 22 */             } catch (InterruptedException e) {
-/* 23 */               System.out.println(isInterrupted());
-/* 24 */               e.printStackTrace();
-/*    */             } 
-/*    */           } 
-/*    */         }
-/*    */       };
-/*    */     
-/* 30 */     thread.start();
-/* 31 */     Thread.sleep(1000L);
-/* 32 */     System.out.println(thread.isInterrupted());
-/* 33 */     thread.interrupt();
-/* 34 */     System.out.println(thread.isInterrupted());
-/*    */   }
-/*    */ }
+package concurrency.chapter6;
 
 
-/* Location:              F:\idea_workspace\demo\concurrency\target\classes\!\com\comcurrency\chapter6\ThreadInterrupt.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.2
- */
+public class ThreadInterrupt {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        while (true) {
+                            wait(100L);
+                            break;
+                        }
+                    } catch (InterruptedException e) {
+                        System.out.println(isInterrupted());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        thread.start();
+        Thread.sleep(1000L);
+        System.out.println(thread.isInterrupted());
+        thread.interrupt();
+        System.out.println(thread.isInterrupted());
+    }
+}
+
--- src/main/java/concurrency/chapter9/ProduceConsumerVersion3.java
+++ src/main/java/concurrency/chapter9/ProduceConsumerVersion3.java
@@ -4,13 +4,13 @@ package concurrency.chapter9;
 import java.util.stream.Stream;
 
 /***
- * @description: 在多个线程生产和消费的情况下会产生线程全部等待的情况
+ * @description:
  * @param
  * @return:
  * @author: Andy
  * @date: 2019/11/20  14:32
  */
-public class ProduceConsumerVersion2 {
+public class ProduceConsumerVersion3 {
     private int i = 1;
 
     private final Object LOCK = new Object();
@@ -19,41 +19,42 @@ public class ProduceConsumerVersion2 {
 
 
     private void produce() {
-        synchronized (this.LOCK) {
-            if (this.pflag) {
+        synchronized (LOCK) {
+           while (pflag) {  // 如果使用if    两个线程被同时唤醒  但是只有一个能拿到锁  假如第一个线程拿到锁 被唤醒 继续往下执行生产完毕后 whit    第二个唤醒后也会继续执行 重复生产
+               //使用while的话会 再次检查 flag  此时以第一个线程生产后已为true  所以不会再次生产 避免重复生产
                 try {
-                    this.LOCK.wait();
+                    LOCK.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            } else {
-                this.i++;
-                System.out.println("P  -->" + this.i);
-                this.pflag = true;
-                this.LOCK.notify();
             }
+            i++;
+            System.out.println(Thread.currentThread().getName() +" ---->  " + i);
+            pflag = true;
+            LOCK.notifyAll();
         }
     }
 
     private void consumer() {
-        synchronized (this.LOCK) {
-            if (this.pflag) {
-                System.out.println("C  -->" + this.i);
-                this.LOCK.notify();
-                this.pflag = false;
-            } else {
+        synchronized (LOCK) {
+            //如果没有数据  则what
+            while (!pflag) {
                 try {
-                    this.LOCK.wait();
+                    LOCK.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
             }
+            System.out.println(Thread.currentThread().getName() + this.i);
+            LOCK.notifyAll();
+            pflag = false;
         }
     }
 
 
     public static void main(String[] args) {
-        final ProduceConsumerVersion2 pc = new ProduceConsumerVersion2();
+        final ProduceConsumerVersion3 pc = new ProduceConsumerVersion3();
        Stream.of("P1","P2").forEach(n -> new Thread(n) {
            @Override
            public void run() {
